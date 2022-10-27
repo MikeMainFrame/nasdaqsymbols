be24ed677863fb49d7898092c81a4f29a041989d
Michael Kenneth Rasch <miketriticum@gmail.com>
2022-10-27 11:06:45 +0200

    20221027002 - hygiene and missing components

---
+++ timestamp.js
@@ -0,0 +1,10 @@
+module.exports = function timestamp( milliseconds = new Date() ) {
+
+  let date = new Date( milliseconds );
+  
+  return ( date.getUTCFullYear() * 1e8 )
+    + ( ( date.getMonth() + 1 ) * 1e6 )
+    + ( ( date.getDate() ) * 1e4 )
+    + ( date.getHours() * 1e2 )
+    + date.getMinutes()
+}
